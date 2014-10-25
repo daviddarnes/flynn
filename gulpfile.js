7a070c28373f6fb07950c31d5505dcfed2e1fab9
David Darnes <me@daviddarnes.com>
2014-10-25 15:27:06 +0100

    Adds demo task

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,17 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 
+// Final CSS task
 gulp.task('less', function () {
 	gulp.src('./less/grid.less')
 	.pipe(less())
 	.pipe(gulp.dest('./'));
 });
+
+// Demo page task
+gulp.task('demo', function () {
+	gulp.src(['./less/grid.less', './less/demo.less'])
+	.pipe(less())
+	.pipe(gulp.dest('./demo/'));
+});
+
